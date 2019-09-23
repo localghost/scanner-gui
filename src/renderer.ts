d412554de5a2a187487708328bce97b5ac57ad4a
Zbigniew Kostrzewa <zkostrzewa@gmail.com>
2019-09-23 15:04:37 +0200

    open file from renderer

--- src/renderer.ts
+++ src/renderer.ts
@@ -2,11 +2,21 @@
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
 
-import { ipcRenderer } from "electron";
+import { ipcRenderer, dialog, remote } from "electron";
+import * as fs from "fs";
 
 console.log("inside renderer");
 
 ipcRenderer.on("open-file", (event, files) => {
   console.log(files);
   document.getElementById("file-list").innerHTML += files;
+});
+
+function addFile() {
+  console.log("clicked");
+  remote.dialog.showOpenDialogSync({properties: ["openFile"]});
+}
+
+document.querySelector('#add-file').addEventListener('click', () => {
+  addFile();
 });
