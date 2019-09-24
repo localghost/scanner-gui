f617ffb3e9f260befaca1f91c6d1c5bf7103bf4f
Zbigniew Kostrzewa <zkostrzewa@gmail.com>
2019-09-24 06:41:23 +0200

    improve menu

--- src/renderer.ts
+++ src/renderer.ts
@@ -2,8 +2,9 @@
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
 
-import { ipcRenderer, dialog, remote } from "electron";
+import { ipcRenderer, dialog, remote, app } from "electron";
 import * as fs from "fs";
+import * as path from "path";
 
 console.log("inside renderer");
 
@@ -12,11 +13,35 @@ ipcRenderer.on("open-file", (event, files) => {
   document.getElementById("file-list").innerHTML += files;
 });
 
-function addFile() {
-  console.log("clicked");
-  remote.dialog.showOpenDialogSync({properties: ["openFile"]});
+function appendFiles(paths: string[]) {
+    const fileList = document.querySelector("#file-list .list");
+    for (const p of paths) {
+        fileList.innerHTML += `
+            <li ripple>
+                <span class="item-text">
+                    ${path.basename(p)}
+                    <span class="secondary-text">${p}</span>
+                </span>
+                <i class="icon-highlight-remove item-action"></i>
+            </li>
+        `;
+    }
 }
 
-document.querySelector('#add-file').addEventListener('click', () => {
-  addFile();
-});
+function addFile() {
+  console.log("clicked");
+  appendFiles(remote.dialog.showOpenDialogSync({ properties: ["openFile"] }));
+}
+
+function addFolder() {
+  console.log("clicked");
+  appendFiles(remote.dialog.showOpenDialogSync({ properties: ["openDirectory"] }));
+}
+
+function quit() {
+  remote.app.quit();
+}
+
+document.querySelector("#add-file").addEventListener("click", addFile);
+document.querySelector("#add-folder").addEventListener("click", addFolder);
+document.querySelector("#app-quit").addEventListener("click", quit);
